5e8be558864535c9fb6afb1c42b64edfffa03ec9
Eduardo <eduardopiasson7@gmail.com>
2020-03-05 16:55:45 -0300

    JS asynchronous - AJAX request

--- Todos/todos.js
+++ Todos/todos.js
@@ -2,7 +2,6 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-
 var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
