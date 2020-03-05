22a5cbb54d5d2b7b0e3d3c022354af0a391f4906
Eduardo <eduardopiasson7@gmail.com>
2020-03-05 15:58:39 -0300

    start aplications

--- todos.js
+++ todos.js
@@ -1,1 +1,9 @@
-alert('Funcionou!');
+var listELement = document.querySelector('#app ul');
+var imputElement = document.querySelector('#app input');
+var buttonELement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer café',
+    'Estudar JavaScript',
+    'Acessar comunidade da Rocktseat'
+];
