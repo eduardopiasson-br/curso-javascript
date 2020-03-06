a2216e084b80e76ead91f66f6c7308cb37abf6aa
Eduardo <eduardopiasson7@gmail.com>
2020-03-06 15:23:05 -0300

    ES6 - Class

--- ES6/main.js
+++ ES6/main.js
@@ -1,11 +1,7 @@
-alert('Teste');
-
-class Teste {
-    metodo() {
-
-    }
-
-    outro() {
-        
+class Matematica {
+    static soma(a, b) {
+        return a + b;
     }
 }
+
+console.log(Matematica.soma(1,2));
