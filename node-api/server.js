e161eeec4cf901d14b2c640fa8097145eee78789
Eduardo Piasson <eduardo_piasson@bitbucket.com>
2020-03-12 15:43:17 -0300

    NodeJS - First Product on DB

--- node-api/server.js
+++ node-api/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', 
+    { useNewUrlParser: true },
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+//Primeira rota
 app.get('/', (req, res) => {
-    res.send('Hello Rocketseat');
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'https://github.com/facebook/react-native'
+    });
+    return res.send('Hello Rocketseat');
 });
 
 app.listen(3001);
