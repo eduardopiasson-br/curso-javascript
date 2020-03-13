e30979ffe46db3afc7c4dfd4f541c9ec5379904e
Eduardo Piasson <eduardo_piasson@bitbucket.com>
2020-03-13 11:38:29 -0300

    NodeJS - CRUD, Insomnia, MVC, pagination

--- node-api/server.js
+++ node-api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 //Iniciando o App
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 //Iniciando o DB
 mongoose.connect(
@@ -12,15 +15,7 @@ mongoose.connect(
 );
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
 //Rotas
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'https://github.com/facebook/react-native'
-    });
-return res.send('Hello Rocketseat');
-});
+app.use('/api', require('./src/routes'));
+
 app.listen(3001);
